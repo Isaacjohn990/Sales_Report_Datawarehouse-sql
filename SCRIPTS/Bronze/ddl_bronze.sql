80bd5f3153eab667c532c440bcff575a0449231c
Isaac John <isaaczachariahjohn@gmail.com>
2026-05-08 14:57:03 +0100

    Create Table for Bronze layer

--- SCRIPTS/Bronze/ddl_bronze.sql
+++ SCRIPTS/Bronze/ddl_bronze.sql
@@ -11,6 +11,7 @@ RUN this script to re-define the DDL structure of 'bronze' Tables
 
 
 --- Creating Table for Bronze Layer
+
 IF OBJECT_ID ('Bronze_layer.customers', 'U') IS NOT NULL 	
 DROP TABLE Bronze_layer.customers;
 
@@ -86,87 +87,6 @@ CREATE TABLE bronze_layer.Products (
 	);
 GO
 
-
---- Creating Stored Procedure to load data into Bronze_Layer Table
-EXEC  Bronze_layer.load_Bronze
-
-CREATE OR ALTER PROCEDURE Bronze_layer.load_Bronze
-AS
-BEGIN
-
-PRINT '-------------------------------------------------';
-PRINT 'loading Data into tables';
-PRINT '-------------------------------------------------';
-
--- For Customers Table
-TRUNCATE TABLE bronze_layer.customers; 
-
-BULK INSERT bronze_layer.customers
-FROM "C:\Users\USER\Documents\New dataset - sql-warehouse\Customers.csv"
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	ROWTERMINATOR = '\n',
-	TABLOCK
-);
-
-SELECT COUNT (*) FROM bronze_layer.customers;
-
--- For Employees Table
-TRUNCATE TABLE bronze_layer.Employees;
-
-BULK INSERT bronze_layer.Employees
-FROM "C:\Users\USER\Documents\New dataset - sql-warehouse\Employees.csv"
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	ROWTERMINATOR = '\n',
-	TABLOCK
-);
-
-SELECT COUNT (*) FROM bronze_layer.Employees;
-
--- For Orders Table
-TRUNCATE TABLE bronze_layer.Orders;
-
-BULK INSERT bronze_layer.Orders
-FROM "C:\Users\USER\Documents\New dataset - sql-warehouse\Orders.csv"
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	ROWTERMINATOR = '\n',
-	TABLOCK
-);
-SELECT COUNT (*) FROM bronze_layer.Orders;
-
--- For OrdersArchive Table
-TRUNCATE TABLE bronze_layer.OrdersArchive
-
-BULK INSERT bronze_layer.OrdersArchive
-FROM "C:\Users\USER\Documents\New dataset - sql-warehouse\OrdersArchive.csv"
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	ROWTERMINATOR = '\n',
-	TABLOCK
-);
-
-SELECT COUNT (*) FROM bronze_layer.OrdersArchive;
-
--- For Products Table
-TRUNCATE TABLE bronze_layer.Products;
-
-BULK INSERT bronze_layer.Products
-FROM "C:\Users\USER\Documents\New dataset - sql-warehouse\Products.csv"
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	ROWTERMINATOR = '\n',
-	TABLOCK
-);
-SELECT COUNT (*) FROM bronze_layer.Products;
-
-END
 
 
 
