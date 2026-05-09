61dcb84e81fa888d672c6e4077b76d54c1db5c72
Isaac John <isaaczachariahjohn@gmail.com>
2026-05-09 15:18:01 +0100

    Create bronze layer  table for CSV bulk load
    Create initial Bronze layer tables and supporting stored procedure to ingest raw data from CSV files.

--- SCRIPTS/Bronze/ddl_bronze.sql
+++ SCRIPTS/Bronze/ddl_bronze.sql
@@ -7,81 +7,7 @@ Scripts Purpose:
     tables if they already exist.
 RUN this script to re-define the DDL structure of 'bronze' Tables
 =====================================================================
-
 */
---- Creating Stored Procedure to load data into Bronze_Layer Table
-EXEC  Bronze_layer.load_Bronze
-
-CREATE OR ALTER PROCEDURE Bronze_layer.load_Bronze
-AS
-BEGIN
-
-PRINT '-------------------------------------------------';
-PRINT 'loading Data into tables';
-PRINT '-------------------------------------------------';
-
--- For Customers Table
-TRUNCATE TABLE bronze_layer.customers; 
-
-BULK INSERT bronze_layer.customers
-FROM "C:\Users\USER\Documents\New dataset - sql-warehouse\Customers.csv"
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	ROWTERMINATOR = '\n',
-	TABLOCK
-);
-
-SELECT COUNT (*) FROM bronze_layer.customers;
-
--- For Employees Table
-TRUNCATE TABLE bronze_layer.Employees;
-
-BULK INSERT bronze_layer.Employees
-FROM "C:\Users\USER\Documents\New dataset - sql-warehouse\Employees.csv"
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	ROWTERMINATOR = '\n',
-	TABLOCK
-);
-
-SELECT COUNT (*) FROM bronze_layer.Employees;
-
--- For Orders Table
-TRUNCATE TABLE bronze_layer.Orders;
-
-BULK INSERT bronze_layer.Orders
-FROM "C:\Users\USER\Documents\New dataset - sql-warehouse\Orders.csv"
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	ROWTERMINATOR = '\n',
-	TABLOCK
-);
-SELECT COUNT (*) FROM bronze_layer.Orders;
-
--- For OrdersArchive Table
-TRUNCATE TABLE bronze_layer.OrdersArchive
-
-BULK INSERT bronze_layer.OrdersArchive
-FROM "C:\Users\USER\Documents\New dataset - sql-warehouse\OrdersArchive.csv"
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	ROWTERMINATOR = '\n',
-	TABLOCK
-);
-
-SELECT COUNT (*) FROM bronze_layer.OrdersArchive;
-
--- For Products Table
-TRUNCATE TABLE bronze_layer.Products;
-
-BULK INSERT bronze_layer.Products
-FROM "C:\Users\USER\Documents\New dataset - sql-warehouse\Products.csv"
-WI====================================================
-
 
 
 --- Creating Table for Bronze Layer
@@ -160,6 +86,8 @@ CREATE TABLE bronze_layer.Products (
 	Price INT
 	);
 GO
+
+
 
 
 
